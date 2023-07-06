a45927a708a8babce52042ac98f749f21fd34e85
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-05 23:08:38 -0300

    Searchbar simplificada

--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -1,5 +1,5 @@
 import "./style.scss";
 const SearchBar = ({ text, icon }) => {
-  return <>{icon ? <input id={"icon"} className="searchBar" type="search" placeholder={text} /> : <input className="searchBar" type="search" placeholder={text} />}</>;
+  return <input id={icon ? "icon" : undefined} className="searchBar" type="search" placeholder={text} />;
 };
 export default SearchBar;
