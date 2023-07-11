48390a7b468d62c7a27cf48eec87d1ba695ed10f
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-11 18:04:06 -0300

    Drawer funcional, componentização do texto com flecha

--- src/components/mobileDrawer/index.jsx
+++ src/components/mobileDrawer/index.jsx
@@ -1,5 +1,10 @@
 import "./style.scss";
 import Modal from "../modal";
+import profileImg from "../../assets/icon/bag.svg"; //placeholder
+import ArrowSvg from "../../assets/icon/ArrowSvg";
+import TextAndArrow from "../textAndArrow";
+
+const nome = "Vicente"; //placeholder
 
 const MobileDrawer = ({ setOpen, open }) => {
   return (
@@ -8,24 +13,35 @@ const MobileDrawer = ({ setOpen, open }) => {
       <div className={!open ? "drawer" : "drawer drawerOpen"}>
         {open && (
           <>
-            <div></div>
-            <div className="separador"></div>
-            <div>
-              <h1>Top Categories</h1>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="profileDiv">
+              <div>
+                <img src={profileImg} className="imgOutline" alt="nome"></img>
+                <span>{`Hello, ${nome}`}</span>
+              </div>
+              <ArrowSvg stroke="black" x={-90} />
             </div>
             <div className="separador"></div>
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="subTitle">
+              <span>Top Categories</span>
+            </div>
+            <div className="listContainer">
+              <TextAndArrow x={-90} text="Skincare" />
+              <TextAndArrow x={-90} text="Apparels" />
+              <TextAndArrow x={-90} text="Jwellery" />
+              <TextAndArrow x={-90} text="Handbags" />
+              <TextAndArrow x={-90} text="Eyeware" />
+              <TextAndArrow x={-90} text="Fragrance" />
+              <TextAndArrow x={-90} text="Watches" />
+              <TextAndArrow x={-90} text="About" />
+            </div>
+            <div className="separador"></div>
+            <div className="subTitle">
+              <span>Contact Us</span>
+            </div>
+            <div className="listContainer contact">
+              <TextAndArrow stroke="black" x={-90} text="Help & Support" />
+              <TextAndArrow stroke="black" x={-90} text="Feedback & Suggestions" />
+              <TextAndArrow stroke="black" x={-90} text="Visit Wbsites" />
             </div>
           </>
         )}
