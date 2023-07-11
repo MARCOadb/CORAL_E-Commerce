395ba9e3769c226d155b5af5e00a55ae6cc0910c
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-11 12:03:48 -0300

    Modal concluido, animação do drawer e modal

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,43 +9,52 @@ import profileIcon from "../../assets/icon/profile.svg";
 import bagIcon from "../../assets/icon/bag.svg";
 import useBreakpoint from "../../hooks/useBreakPoint";
 import SearchBar from "../searchBar";
+import { useState } from "react";
+import MobileDrawer from "../mobileDrawer";
+import Modal from "../modal";
 
 const Header = () => {
   const { phone, desktop } = useBreakpoint();
-
+  const [open, setOpen] = useState(false);
   return (
-    <div className="headerContainer">
-      {desktop ? (
-        <>
-          <img src={coralLogo} />
-          <div className="navContainer">
-            <button className="redirectBtn">Handbags</button>
-            <button className="redirectBtn">Watches</button>
-            <button className="redirectBtn">Skincare</button>
-            <button className="redirectBtn">Jewellery</button>
-            <button className="redirectBtn">Apparels</button>
-          </div>
-          <SearchBar text={"Search for products or brands....."} icon={true} />
-          <div className="navContainer">
-            <img src={wishlistIcon} alt="wishlistIcon" />
-            <img src={profileIcon} alt="profileIcon" />
-            <img src={bagIcon} alt="bagIcon" />
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="navContainer">
-            <img src={menuIcon} alt="menuIcon" />
-            <span>Home</span>
-          </div>
-          <div className="navContainer">
-            <img src={addIcon} alt="addIcon" />
-            <img src={searchIcon} alt="searchIcon" />
-            <img src={notificationIcon} alt="notificationIcon" />
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      
+      <MobileDrawer setOpen={setOpen} open={open} />
+      <div className="headerContainer">
+        {desktop ? (
+          <>
+            <img src={coralLogo} />
+            <div className="navContainer">
+              <button className="redirectBtn">Handbags</button>
+              <button className="redirectBtn">Watches</button>
+              <button className="redirectBtn">Skincare</button>
+              <button className="redirectBtn">Jewellery</button>
+              <button className="redirectBtn">Apparels</button>
+            </div>
+            <SearchBar text={"Search for products or brands....."} icon={true} />
+            <div className="navContainer">
+              <img src={wishlistIcon} alt="wishlistIcon" />
+              <img src={profileIcon} alt="profileIcon" />
+              <img src={bagIcon} alt="bagIcon" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="navContainer">
+              <div onClick={() => setOpen(true)}>
+                <img src={menuIcon} alt="menuIcon" />
+              </div>
+              <span>Home</span>
+            </div>
+            <div className="navContainer">
+              <img src={addIcon} alt="addIcon" />
+              <img src={searchIcon} alt="searchIcon" />
+              <img src={notificationIcon} alt="notificationIcon" />
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 export default Header;
--- src/components/mobileDrawer/index.jsx
+++ src/components/mobileDrawer/index.jsx
@@ -1,7 +1,36 @@
-import { useEffect } from "react";
 import "./style.scss";
+import Modal from "../modal";
 
-const MobileDrawer = () => {
-  return <></>;
+const MobileDrawer = ({ setOpen, open }) => {
+  return (
+    <>
+      <Modal setOpen={setOpen} open={open} />
+      <div className={!open ? "drawer" : "drawer drawerOpen"}>
+        {open && (
+          <>
+            <div></div>
+            <div className="separador"></div>
+            <div>
+              <h1>Top Categories</h1>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="separador"></div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 export default MobileDrawer;
