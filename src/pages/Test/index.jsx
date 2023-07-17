21c2384a9d154d4ce34dad2b3c557f43a7e3a1c1
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-17 19:30:55 -0300

    Modal mobile ancorado em baixo, funcional estilizado utiliza da mesma logica que os layouts

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
 import ArrowSvg from "../../assets/icon/ArrowSvg";
 import CrossSvg from "../../assets/icon/CrossSvg";
 import Header from "../../components/header";
 import MobileLayout from "../../layouts/mobileLayout";
-import getProductById from "../../services/getProductById";
-import getProducts from "../../services/getProducts";
 
 const sla = () => console.log("aaaaa");
 const botoes = [
