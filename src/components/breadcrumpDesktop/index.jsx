1c4b414a76c3ee2145c7da9711170e9c31766f00
VicenteBp <vicentebpberwanger@gmail.com>
2023-07-20 18:03:45 -0300

    Resolução do bug das tags

--- src/components/breadcrumpDesktop/index.jsx
+++ src/components/breadcrumpDesktop/index.jsx
@@ -4,52 +4,46 @@ import getCategoryById from "../../services/getCategoryById";
 import { useEffect, useState } from "react";
 
 const Breadcrump = () => {
-    const location = useLocation()
-    const [category, setCategory] = useState(null)
-    const [loading, setLoading] = useState(false)
+  const location = useLocation();
+  const [category, setCategory] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const callCategory = async(id) =>{
-      setLoading(true)
-      const cat = await getCategoryById(id)
-      return cat.name
-    }
-    useEffect(()=>{
-      callCategory()
-      .then((data)=>{
-        setCategory(data)
+  const callCategory = async (id) => {
+    setLoading(true);
+    const cat = await getCategoryById(id);
+    return cat.name;
+  };
+  useEffect(() => {
+    callCategory()
+      .then((data) => {
+        setCategory(data);
       })
-      .finally(()=>{
-        setLoading(false)
-      })
-    },[])
-  
-    let currentLink = ''
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-    const crumbs = location.pathname.split('/')
-    .filter(crumb => crumb !== '')
-    .map(crumb => {
-        currentLink =+ '/${crumb}'
-        return (
-            <div className="crumb" key={crumb}>
-              <Link to {...currentLink}>{crumb}</Link>
-            </div>
-          )
-        })
-    return (
-      
-        <div className="breadcrumbs">
-        <>
-            {crumbs}
-        <span><span/>
-        </>
-          
+  let currentLink = "";
+
+  const crumbs = location.pathname
+    .split("/")
+    .filter((crumb) => crumb !== "")
+    .map((crumb) => {
+      currentLink = +"/${crumb}";
+      return (
+        <div className="crumb" key={crumb}>
+          <Link to {...currentLink}>
+            {crumb}
+          </Link>
         </div>
-        
-    ) 
-      }
-    
+      );
+    });
+  return (
+    <div className="breadcrumbs">
+      {crumbs}
+      <span></span>
+    </div>
+  );
+};
 
-
-
-  
-  export default Breadcrump;
+export default Breadcrump;
