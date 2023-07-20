b0ac28922492482e37811e811e343540f9ea0bd4
Lucas <lucas123melo2017@gmail.com>
2023-07-20 17:56:20 -0300

    breadcrump not finished

---
+++ src/components/breadcrumpDesktop/index.jsx
@@ -0,0 +1,55 @@
+import { Link, useLocation } from "react-router-dom";
+import "./style.scss";
+import getCategoryById from "../../services/getCategoryById";
+import { useEffect, useState } from "react";
+
+const Breadcrump = () => {
+    const location = useLocation()
+    const [category, setCategory] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const callCategory = async(id) =>{
+      setLoading(true)
+      const cat = await getCategoryById(id)
+      return cat.name
+    }
+    useEffect(()=>{
+      callCategory()
+      .then((data)=>{
+        setCategory(data)
+      })
+      .finally(()=>{
+        setLoading(false)
+      })
+    },[])
+  
+    let currentLink = ''
+
+    const crumbs = location.pathname.split('/')
+    .filter(crumb => crumb !== '')
+    .map(crumb => {
+        currentLink =+ '/${crumb}'
+        return (
+            <div className="crumb" key={crumb}>
+              <Link to {...currentLink}>{crumb}</Link>
+            </div>
+          )
+        })
+    return (
+      
+        <div className="breadcrumbs">
+        <>
+            {crumbs}
+        <span><span/>
+        </>
+          
+        </div>
+        
+    ) 
+      }
+    
+
+
+
+  
+  export default Breadcrump;
--- src/pages/Categories/index.jsx
+++ src/pages/Categories/index.jsx
@@ -1,8 +1,10 @@
+import Breadcrump from "../../components/breadcrumpDesktop";
 import NavBarMobile from "../../components/navBarMobile";
 export default function Category() {
     return (
         <>
         <h1>Página category</h1>
+        <Breadcrump />
         <NavBarMobile />
         </>
     )
