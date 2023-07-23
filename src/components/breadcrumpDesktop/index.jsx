7323ecde7be21e948f143354de7282fa6d2a848a
Lucas <lucas123melo2017@gmail.com>
2023-07-23 19:09:54 -0300

    Breadcrump finalizado

--- src/components/breadcrumpDesktop/index.jsx
+++ src/components/breadcrumpDesktop/index.jsx
@@ -6,57 +6,24 @@ import getProductById from "../../services/getProductById";
 import ChevronRightSmallsvg from "../../assets/icon/ChevronRightSmallsvg"
 
 
-const Breadcrump = ({idCategory, idProduct}) => {
-  const location = useLocation();
-  const [category, setCategory] = useState(null);
-  const [label, setLabel] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const breadCrump = async (idCategory, idProduct) => {
-    setLoading(true);
-    if(idProduct){
-      const category = await getCategoryById(idCategory);
-      const product = await getProductById(idProduct);
-      return ({
-        category : category.name,
-        product : product.name, 
-      })
-    }
-    else if(idCategory){
-      const category = await getCategoryById(idCategory);
-      return ({
-        category : category.name,
-      })
-    }
-    
-  };
-  useEffect(() => {
-    breadCrump(idCategory,idProduct)
-
-      .then((data) => {
-        setCategory(data?.category);
-        setLabel(data?.product);
-      })
-      
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
+const Breadcrump = ({category, product, page}) => {
   const navigate = useNavigate();
   const handlePage = () => {
-    navigate(page);
+    navigate(`/${page}`);
   };
   const handleCategory = () => {
-    navigate(category);
-  }
+    // navigate(`/${page}/${category}`);
+  };
+
   return (
     <div className="breadcrumbs">
     <>
-      <div onClick={handlePage}>{page}</div>
+      <span onClick={handlePage}>{page}</span>
       <ChevronRightSmallsvg />
-      <div onClick={label?(handleCategory):("")} style={label?({}):({ color: "#626262" })}>{category}</div>
-      {label && <ChevronRightSmallsvg />}
-      <div style={{ color: '#626262' }}>{label}</div>
+      {/* <span onClick={product?(handleCategory):("")} style={product?({}):({ color: "#626262" })}>{category}</span> */}
+      <span style={product?({}):({ color: "#626262" })}>{category}</span>
+      {product && <ChevronRightSmallsvg />}
+      <span style={{ color: '#626262' }}>{product}</span>
     </>
 </div>
 
