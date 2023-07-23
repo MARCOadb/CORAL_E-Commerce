ef01d1acd7bce3d4f1e4de96c3f5cd3b787577c4
VicenteBp <vicentebpberwanger@gmail.com>
2023-07-23 20:44:51 -0300

    Remoção de imports não usados

--- src/components/breadcrumpDesktop/index.jsx
+++ src/components/breadcrumpDesktop/index.jsx
@@ -1,12 +1,8 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./style.scss";
-import getCategoryById from "../../services/getCategoryById";
-import { useEffect, useState } from "react";
-import getProductById from "../../services/getProductById";
-import ChevronRightSmallsvg from "../../assets/icon/ChevronRightSmallsvg"
+import ChevronRightSmallsvg from "../../assets/icon/ChevronRightSmallsvg";
 
-
-const Breadcrump = ({category, product, page}) => {
+const Breadcrump = ({ category, product, page }) => {
   const navigate = useNavigate();
   const handlePage = () => {
     navigate(`/${page}`);
@@ -17,16 +13,15 @@ const Breadcrump = ({category, product, page}) => {
 
   return (
     <div className="breadcrumbs">
-    <>
-      <span onClick={handlePage}>{page}</span>
-      <ChevronRightSmallsvg />
-      {/* <span onClick={product?(handleCategory):("")} style={product?({}):({ color: "#626262" })}>{category}</span> */}
-      <span style={product?({}):({ color: "#626262" })}>{category}</span>
-      {product && <ChevronRightSmallsvg />}
-      <span style={{ color: '#626262' }}>{product}</span>
-    </>
-</div>
-
+      <>
+        <span onClick={handlePage}>{page}</span>
+        <ChevronRightSmallsvg />
+        {/* <span onClick={product?(handleCategory):("")} style={product?({}):({ color: "#626262" })}>{category}</span> */}
+        <span style={product ? {} : { color: "#626262" }}>{category}</span>
+        {product && <ChevronRightSmallsvg />}
+        <span style={{ color: "#626262" }}>{product}</span>
+      </>
+    </div>
   );
 };
 
