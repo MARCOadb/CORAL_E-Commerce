81ab04457a030ed8e3dcd308b2e563144ac7e58d
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-27 16:09:18 -0300

    Services para setar o produto, adcionar e remover no objeto que contem id e qnt

--- src/services/deleteBagProduct.js
+++ src/services/deleteBagProduct.js
@@ -1,8 +1,13 @@
 export const deleteBagProduct = (productId) => {
   if (localStorage.getItem("bag") === null) localStorage.setItem("bag", "[]");
   const bagArr = JSON.parse(localStorage.getItem("bag"));
-  const index = bagArr.findIndex((item) => item === productId);
-  if (index >= 0) bagArr.splice(index, 1);
-
-  localStorage.setItem("bag", JSON.stringify(bagArr));
+  const productFound = bagArr.find((item) => item.id === productId);
+  if (productFound?.qnt > 1) {
+    productFound.qnt--;
+    localStorage.setItem("bag", JSON.stringify(bagArr));
+  } else {
+    const removedItem = bagArr.filter((item) => item.qnt !== 1);
+    console.log(removedItem);
+    localStorage.setItem("bag", JSON.stringify(removedItem));
+  }
 };
