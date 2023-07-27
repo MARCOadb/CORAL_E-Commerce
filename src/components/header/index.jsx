2e6862c69a1e8315a5422a348db1838451112170
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-27 16:11:50 -0300

    Correção de conflitos e key no componente produto

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,15 +11,12 @@ import useBreakpoint from "../../hooks/useBreakPoint";
 import SearchBar from "../searchBar";
 import { useState } from "react";
 import MobileDrawer from "../mobileDrawer";
-<<<<<<< HEAD
 import Modal from "../modal";
 import WishlistSvg from "../../assets/icon/WishlistSvg";
 import ProfileSvg from "../../assets/icon/Profilesvg";
 import BagSvg from "../../assets/icon/Bagsvg";
 import HeaderModal from "../headerModal";
-=======
 import { useLocation, useNavigate } from "react-router-dom";
->>>>>>> d34887067093f924482ac3f530a7e0728d90a652
 
 const Header = ({ path }) => {
   const { phone, desktop } = useBreakpoint();
@@ -36,14 +33,8 @@ const Header = ({ path }) => {
   };
   return (
     <>
-<<<<<<< HEAD
-      <div className="headerContainer">
-        {desktop ? (
-          <>
-            <HeaderModal setOpen={setOpen} open={open} />
-            <img src={coralLogo} />
-=======
-      <MobileDrawer setOpen={setOpen} open={open} />
+      <HeaderModal setOpen={setOpen} open={open} />
+
       <div className="headerContainer">
         {desktop ? (
           <>
@@ -53,7 +44,6 @@ const Header = ({ path }) => {
                 navigate("/home", { state: { path: "home" } });
               }}
             />
->>>>>>> d34887067093f924482ac3f530a7e0728d90a652
             <div className="navContainer">
               <button
                 className="redirectBtn"
@@ -98,9 +88,9 @@ const Header = ({ path }) => {
             </div>
             <SearchBar text={"Search for products or brands....."} icon={true} />
             <div className="navContainer">
-              <WishlistSvg onClick={() => setOpen(true)} />
+              <WishlistSvg />
               <ProfileSvg stroke={"#1B4B66"} />
-              <BagSvg stroke={"#1B4B66"} />
+              <BagSvg onClick={() => setOpen(true)} stroke={"#1B4B66"} />
             </div>
           </>
         ) : (
