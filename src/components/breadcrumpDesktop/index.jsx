5329db3ed163c5415d1ff02e3e35e242bab6d682
Lucas <lucas123melo2017@gmail.com>
2023-07-28 18:14:10 -0300

    Finalização do Breadcrump

--- src/components/breadcrumpDesktop/index.jsx
+++ src/components/breadcrumpDesktop/index.jsx
@@ -6,16 +6,22 @@ const Breadcrump = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const handleCategory = () => {
-    // navigate(`/${page}/${category}`);
+  const handleClick = ({category, path}) => {
+    if(path && category){
+    navigate(`/${path}/${category}`);
+    }
+    else if(path){
+    navigate(`/${path}`);
+    }
+
   };
 
   return (
     <div className="breadcrumbs">
       <>
-        <span>{location.state?.path}</span>
+        <span onClick={()=>handleClick({path:location.state?.path})}>{location.state?.path}</span>
         <ChevronRightSmallsvg />
-        <span style={location.state?.product ? {} : { color: "#626262" }}>{location.state?.category}</span>
+        <span onClick={location.state?.product?()=>handleClick({path:location.state?.path, category:location.state?.category}):(()=>{})} style={location.state?.product ? {} : { color: "#626262" }}>{location.state?.category}</span>
         {location.state?.product && <ChevronRightSmallsvg />}
         <span style={{ color: "#626262" }}>{location.state?.product}</span>
       </>
