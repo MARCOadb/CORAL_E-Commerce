0c43652afbe16cf3ab6b7f02b38ed90283832c36
Luiza Graminho <luiza.sgraminho@gmail.com>
2023-07-31 04:01:57 -0300

    Parte visual e estilização

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,9 +1,12 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-
+import { Element } from "react-scroll";
+import ProductPhotos from "../../components/productPhotos";
+import QuantityInput from "../../components/quantityButton";
 import useBreakpoint from "../../hooks/useBreakPoint";
-import { useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import Tabs from "../../components/productDescription/slider";
+
 
 import style from "./style.scss";
 import utilityClasses from "../../assets/utility.scss";
@@ -16,6 +19,7 @@ export default function ProductPage(){
         <div className="product-image">
             
         </div>
+       <ProductPhotos/>
        <Tabs/>
         <div className="product-info">
             <div className="product-name-and-description">
@@ -33,11 +37,27 @@ export default function ProductPage(){
                 <div className="pin-code"><input  placeholder="Apply Valid Pincode" className="body-medium"/><button className="title-regular">CHECK</button></div>
                 
             </div>
-            <div className="quantity display-small" color="#13101E">Quantity<input type="button" id="plus" value='-' onclick="process(-1)" />
-  <input id="quant" name="quant" class="text" size="1" type="text" value="1" maxlength="10" />
-  <input type="button" id="minus" value='+' onclick="process(1)"/></div>
+           <QuantityInput/>
+            <Element className="offers">
+                <div className="offers-container">
+                    <h1 className="body-medium">Get upto 30% Off on order value above $100</h1>
+                    <h2 className="body-small">Terms & Conditions</h2>
+                    <div className="offers-code">
+                        <h1 className="body-small">Use Code</h1>
+                        <button className="code body-medium" >ORDER100</button>
+                    </div>
+                </div>
+                <div className="offers-container">
+                    <h1 className="body-medium">Get upto 30% Off on order value above $100</h1>
+                    <h2 className="body-small">Terms & Conditions</h2>
+                    <div className="offers-code">
+                        <h1 className="body-small">Use Code</h1>
+                        <button className="code body-medium" >ORDER100</button>
+                    </div>
+                </div>
+            </Element>
         </div>
-        <Footer/>
+       
        </> 
     )
 }
