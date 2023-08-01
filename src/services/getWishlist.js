d0cf0d674b4f2b09770797a0745c211c14f2d58c
VicenteBp <vicentebpberwanger@gmail.com>
2023-08-01 16:31:57 -0300

    Mudança na getwishlist e fix na categories mobile

--- src/services/getWishlist.js
+++ src/services/getWishlist.js
@@ -1,5 +1,15 @@
-export const getWishlist = (user) => {
-  if (user.wishlist === null) user.wishlist = [];
-  const wishlist = user.wishlist;
-  return wishlist;
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./firebaseConnection";
+
+export const getWishlist = async (userId) => {
+  const wishlistRef = collection(db, "wishlist");
+  const q = query(wishlistRef, where("userId", "==", userId));
+  const querySnapshot = await getDocs(q);
+  if (!querySnapshot.docs[0]) {
+    await addDoc(wishlistRef, {
+      userId,
+      products: [],
+    });
+  }
+  return querySnapshot.docs[0].data().products;
 };
