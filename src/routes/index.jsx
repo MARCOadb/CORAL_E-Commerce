b433fa6a4bf51686610d663f2fff59a9ddecdbb1
VicenteBp <vicentebpberwanger@gmail.com>
2023-08-02 16:24:32 -0300

    Pagina de registro de produtos e service para criação de produtos

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,27 +8,49 @@ import Profile from "../pages/Profile";
 import Test from "../pages/Test";
 import Login from "../pages/Login";
 import useBreakpoint from "../hooks/useBreakPoint";
+import ProductRegister from "../pages/ProductsRegister";
 
 export default function RoutesApp() {
-  const { phone, desktop } = useBreakpoint()
+  const { phone, desktop } = useBreakpoint();
   return (
     <Routes>
       {desktop ? (
         <>
-          <Route path={`/home/profile`} element={<Private><Profile /></Private>} />
+          <Route
+            path={`/home/profile`}
+            element={
+              <Private>
+                <Profile />
+              </Private>
+            }
+          />
           <Route path={`/home/:category`} element={<Categories />} />
           <Route path={`/about/:category`} element={<Categories />} />
         </>
       ) : (
         <>
           <Route path="/categories" element={<Categories />} />
-          <Route path='/profile' element={<Private><Profile /></Private>} />
-          <Route path="/bag" element={<Private><Bag /></Private>} />
+          <Route
+            path="/profile"
+            element={
+              <Private>
+                <Profile />
+              </Private>
+            }
+          />
+          <Route
+            path="/bag"
+            element={
+              <Private>
+                <Bag />
+              </Private>
+            }
+          />
         </>
       )}
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/test" element={<Test />} />
+      <Route path="/test" element={<ProductRegister />} />
       <Route path="/login" element={<Login />} />
       {/* 
         Pagina de produtos
