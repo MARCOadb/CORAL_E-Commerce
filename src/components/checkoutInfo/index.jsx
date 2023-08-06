b0fc6de2f3781c3dc49b01308ba533b4ce981d7b
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2023-08-06 14:20:51 -0300

    Commit fix

--- src/components/checkoutInfo/index.jsx
+++ src/components/checkoutInfo/index.jsx
@@ -1,14 +1,78 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import useBreakpoint from "../../hooks/useBreakPoint";
 
 import "./style.scss";
 
+import Chevron from "../../assets/icon/chevron-bottom.svg";
+
 const CheckoutInfo = () => {
   const { phone, desktop } = useBreakpoint();
 
-  retun(
+  const [toggle, setToggle] = useState(false);
+  const [height, setHeight] = useState();
+
+  const refHeight = useRef();
+
+  useEffect(() => {
+    setHeight(`${refHeight.current.scrollHeight}px`);
+  }, []);
+
+  const toggleState = () => {
+    setToggle(!toggle);
+  };
+
+  return (
     <>
-      <div>{desktop ? <></> : <></>}</div>
+      <div>
+        {desktop ? (
+          <>
+            <button className="checkout-dropdown-button" onClick={toggleState}>
+              <p className="display-small">Add New Adress</p>
+              <img src={Chevron} className={toggle && "chevron-rotate"} />
+            </button>
+            <div className="button-separator"></div>
+            <div className={toggle ? "checkout-dropdown-toggle animation" : "checkout-dropdown-toggle"} style={{ height: toggle ? `${height}` : "0px" }} ref={refHeight}>
+              <div className="new-adress-container">
+                <div className="input-column">
+                  <div className="input-holder">
+                    <p className="body-medium-he">Full Name</p>
+                    <input type="text" placeholder="Enter Name" />
+                  </div>
+                  <div className="input-holder">
+                    <p className="body-medium-he">Street Adress</p>
+                    <input type="text" placeholder="Enter Adress" />
+                  </div>
+                  <div className="input-holder">
+                    <p className="body-medium-he">City</p>
+                    <input type="text" placeholder="Enter City" />
+                  </div>
+                </div>
+                <div className="input-column">
+                  <div className="phone-input-holder">
+                    <p className="body-medium-he">Mobile Number</p>
+                    <div className="phone-input-fields">
+                      <input type="number" placeholder="DDD" className="ddd" />
+                      <input type="number" placeholder="Enter Number" className="number" />
+                    </div>
+                  </div>
+                  <div className="wide-input-holder">
+                    <p className="body-medium-he">State</p>
+                    <input type="text" placeholder="Enter State" />
+                  </div>
+                  <div className="wide-input-holder">
+                    <p className="body-medium-he">Pin Code</p>
+                    <input type="number" placeholder="Enter Pin Code" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1>teste 2!</h1>
+          </>
+        )}
+      </div>
     </>
   );
 };
--- src/components/checkoutPayment/index.jsx
+++ src/components/checkoutPayment/index.jsx
@@ -3,12 +3,58 @@ import useBreakpoint from "../../hooks/useBreakPoint";
 
 import "./style.scss";
 
+import UPI from "../../assets/pics/Payments/upi.png";
+import CDcard from "../../assets/pics/Payments/credit-debit-card.png";
+import ApplePay from "../../assets/pics/Payments/apple-pay.png";
+import AmazonPay from "../../assets/pics/Payments/amazon-pay.png";
+
 const CheckoutPayment = () => {
   const { phone, desktop } = useBreakpoint();
 
-  retun(
+  return (
     <>
-      <div>{desktop ? <></> : <></>}</div>
+      <div>
+        {desktop ? (
+          <>
+            <div className="card-array">
+              <label>
+                <div className="card">
+                  <input type="radio" name="radio" value="option1" />
+                  <img src={UPI} alt="UPI" />
+                  <p className="body-regular-he">UPI</p>
+                  <span></span>
+                </div>
+              </label>
+              <label>
+                <div className="card">
+                  <input type="radio" name="radio" value="option2" />
+                  <img src={CDcard} alt="Credit/Debit Card" />
+                  <p className="body-regular-he">Credit/Debit Card</p>
+                  <span></span>
+                </div>
+              </label>
+              <label>
+                <div className="card">
+                  <input type="radio" name="radio" value="option3" />
+                  <img src={ApplePay} alt="Apple Pay" />
+                  <p className="body-regular-he">Apple Pay</p>
+                  <span></span>
+                </div>
+              </label>
+              <label>
+                <div className="card">
+                  <input type="radio" name="radio" value="option4" />
+                  <img src={AmazonPay} alt="Amazon Pay" />
+                  <p className="body-regular-he">Amazon Pay</p>
+                  <span></span>
+                </div>
+              </label>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 };
