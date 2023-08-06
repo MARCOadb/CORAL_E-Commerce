f498234679bcc5c3edf5c708084dfa86fa3eef19
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2023-08-06 19:38:17 -0300

    Card array micro interactions

--- src/components/checkoutPayment/index.jsx
+++ src/components/checkoutPayment/index.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import useBreakpoint from "../../hooks/useBreakPoint";
+import React, { useState } from "react";
 
 import "./style.scss";
 
@@ -9,51 +8,47 @@ import ApplePay from "../../assets/pics/Payments/apple-pay.png";
 import AmazonPay from "../../assets/pics/Payments/amazon-pay.png";
 
 const CheckoutPayment = () => {
-  const { phone, desktop } = useBreakpoint();
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        {desktop ? (
-          <>
-            <div className="card-array">
-              <label>
-                <div className="card">
-                  <input type="radio" name="radio" value="option1" />
-                  <img src={UPI} alt="UPI" />
-                  <p className="body-regular-he">UPI</p>
-                  <span></span>
-                </div>
-              </label>
-              <label>
-                <div className="card">
-                  <input type="radio" name="radio" value="option2" />
-                  <img src={CDcard} alt="Credit/Debit Card" />
-                  <p className="body-regular-he">Credit/Debit Card</p>
-                  <span></span>
-                </div>
-              </label>
-              <label>
-                <div className="card">
-                  <input type="radio" name="radio" value="option3" />
-                  <img src={ApplePay} alt="Apple Pay" />
-                  <p className="body-regular-he">Apple Pay</p>
-                  <span></span>
-                </div>
-              </label>
-              <label>
-                <div className="card">
-                  <input type="radio" name="radio" value="option4" />
-                  <img src={AmazonPay} alt="Amazon Pay" />
-                  <p className="body-regular-he">Amazon Pay</p>
-                  <span></span>
-                </div>
-              </label>
-            </div>
-          </>
-        ) : (
-          <></>
-        )}
+      <div className="card-array">
+        <label>
+          <div className={`card ${selectedOption === "option1" ? "checked" : ""}`}>
+            <input type="radio" name="radio" value="option1" onChange={handleOptionChange} />
+            <img src={UPI} alt="UPI" />
+            <p className="body-regular-he">UPI</p>
+            <span></span>
+          </div>
+        </label>
+        <label>
+          <div className={`card ${selectedOption === "option2" ? "checked" : ""}`}>
+            <input type="radio" name="radio" value="option2" onChange={handleOptionChange} />
+            <img src={CDcard} alt="Credit/Debit Card" />
+            <p className="body-regular-he">Credit/Debit Card</p>
+            <span></span>
+          </div>
+        </label>
+        <label>
+          <div className={`card ${selectedOption === "option3" ? "checked" : ""}`}>
+            <input type="radio" name="radio" value="option3" onChange={handleOptionChange} />
+            <img src={ApplePay} alt="Apple Pay" />
+            <p className="body-regular-he">Apple Pay</p>
+            <span></span>
+          </div>
+        </label>
+        <label>
+          <div className={`card ${selectedOption === "option4" ? "checked" : ""}`}>
+            <input type="radio" name="radio" value="option4" onChange={handleOptionChange} />
+            <img src={AmazonPay} alt="Amazon Pay" />
+            <p className="body-regular-he">Amazon Pay</p>
+            <span></span>
+          </div>
+        </label>
       </div>
     </>
   );
