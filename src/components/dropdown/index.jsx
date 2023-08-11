1af6eeebd75ed438bb25bbf6b2bbf8a64bd87754
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2023-08-11 06:49:09 -0300

    Switching the footer component to use the dropdown component

--- src/components/dropdown/index.jsx
+++ src/components/dropdown/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import Chevron from "../../assets/icon/chevron-bottom.svg";
+import useBreakpoint from "../../hooks/useBreakPoint";
 
 import "./style.scss";
 
@@ -7,6 +8,7 @@ const Dropdown = ({ title, content }) => {
   const [toggle, setToggle] = useState(false);
   const [height, setHeight] = useState();
   const refHeight = useRef();
+  const { phone, desktop } = useBreakpoint();
 
   useEffect(() => {
     setHeight(`${refHeight.current.scrollHeight}px`);
@@ -18,14 +20,32 @@ const Dropdown = ({ title, content }) => {
 
   return (
     <div>
-      <button className="checkout-dropdown-button" onClick={toggleState}>
-        <p className="display-small">{title}</p>
-        <img src={Chevron} className={toggle ? "chevron-rotate" : undefined} />
-      </button>
-      <div className="button-separator"></div>
-      <div className={toggle ? "checkout-dropdown-toggle animation" : "checkout-dropdown-toggle"} style={{ height: toggle ? `${height}` : "0px" }} ref={refHeight}>
-        {content}
-      </div>
+      {desktop ? (
+        <>
+          <div>
+            <button className="checkout-dropdown-button" onClick={toggleState}>
+              <p className="display-small">{title}</p>
+              <img src={Chevron} className={toggle ? "chevron-rotate" : undefined} />
+            </button>
+            <div className="button-separator"></div>
+            <div className={toggle ? "checkout-dropdown-toggle animation" : "checkout-dropdown-toggle"} style={{ height: toggle ? `${height}` : "0px" }} ref={refHeight}>
+              {content}
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <button className="checkout-dropdown-button" onClick={toggleState}>
+              <p className="title-medium">{title}</p>
+              <img src={Chevron} className={toggle ? "chevron-rotate" : undefined} />
+            </button>
+            <div className={toggle ? "checkout-dropdown-toggle animation" : "checkout-dropdown-toggle"} style={{ height: toggle ? "906px" : "0px" }} ref={refHeight}>
+              {content}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/services/getWishlist.js
+++ src/services/getWishlist.js
@@ -5,20 +5,10 @@ export const getWishlist = async (userId) => {
   const wishlistRef = collection(db, "wishlist");
   const q = query(wishlistRef, where("userId", "==", userId));
   const querySnapshot = await getDocs(q);
-<<<<<<< HEAD
-  //if (!querySnapshot.docs[0]) {
-  //  await addDoc(wishlistRef, {
-  //    userId,
-  //    products: [],
-  //  });
-  //}
-  return querySnapshot.docs[0].data().products;
-=======
   if (querySnapshot.empty) {
     await addDoc(wishlistRef, {
       userId,
       products: [],
     });
   } else return querySnapshot.docs[0].data().products;
->>>>>>> e70852dde7e1f0d7bbfcdccc193a206d66b37b6e
 };
