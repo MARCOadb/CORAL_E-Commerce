0e0b4a385d8bf1cf818f66266679383a528ab5c5
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-08-14 02:51:54 -0300

    My Cart page Fix

--- src/components/cartProduct/index.jsx
+++ src/components/cartProduct/index.jsx
@@ -1,15 +1,15 @@
 import styles from "./style.module.scss";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { addBagProduct } from "../../services/addBagProduct";
 import { BagContext } from "../../contexts/BagContext";
 import CrossSvg from "../../assets/icon/CrossSvg";
 import { deleteBagProduct } from "../../services/deleteBagProduct";
 import { setProductQnt } from "../../services/setProductQnt";
-import MinusSvg from "../../assets/icon/MinusSvg";
-import PlusSvg from "../../assets/icon/PlusSvg";
 import SmallMinus from "../../assets/icon/SmallMinus";
 import SmallPlus from "../../assets/icon/SmallPlus";
 import { AuthContext } from "../../contexts/AuthContext";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../../services/firebaseConnection";
 
 /*PROPS
   {
@@ -24,21 +24,27 @@ import { AuthContext } from "../../contexts/AuthContext";
   }
 */
 
-const CartProduct = ({ data, qnt, stepper, price, remove, itemId, showQnt }) => {
+const CartProduct = ({ data, qnt, stepper, price, remove, itemId, showQnt, largura }) => {
+  const [productImage, setProductImage] = useState(null);
+
   const { update } = useContext(BagContext);
   const { user } = useContext(AuthContext);
+
   const deleteProduct = () => {
     deleteBagProduct(user.uid, itemId, true);
     update({ products: false });
   };
+
   const addProduct = () => {
     addBagProduct(user.uid, itemId);
     update({ products: false });
   };
+
   const removeProduct = () => {
     deleteBagProduct(user.uid, itemId);
     update({ products: false });
   };
+
   const setProduct = (e) => {
     e.preventDefault();
     if (e.key === "Enter") {
@@ -48,14 +54,25 @@ const CartProduct = ({ data, qnt, stepper, price, remove, itemId, showQnt }) => 
     }
   };
 
+  const storageRef = ref(storage, `productsImg/${data.name}`);
+
+  useEffect(() => {
+    const getImages = async () => {
+      await getDownloadURL(storageRef).then((response) => {
+        setProductImage(response);
+      });
+    };
+    getImages();
+  }, []);
+
   return (
-    <div className={styles.cardVertical}>
+    <div className={styles.cardVertical} style={largura && { width: largura + 'px' }}>
       <div style={{ display: "flex", gap: "16px" }}>
-        <img src={data.image} style={{ height: "80px", width: "75px", borderRadius: "8px" }} />
+        <img src={productImage} style={{ height: "80px", width: "75px", borderRadius: "8px", objectFit: 'cover' }} />
         <div className={styles.detailsContainer}>
-          <span>{data.name}</span>
-          <span>{data.description}</span>
-          {showQnt && <span>Qty- {qnt}</span>}
+          <span className="body-medium text-high-emphasis">{data.name}</span>
+          <span className="body-regular text-low-emphasis">{data.description}</span>
+          {showQnt && <span className="body-regular text-low-emphasis">Qty- {qnt}</span>}
           {stepper && (
             <div className={styles.stepperContainer}>
               <SmallMinus onClick={removeProduct} />
@@ -67,7 +84,7 @@ const CartProduct = ({ data, qnt, stepper, price, remove, itemId, showQnt }) => 
       </div>
       <div className={styles.priceContainer}>
         {remove && <CrossSvg stroke={"#626262"} onClick={deleteProduct} />}
-        {price && <span>${data.price}</span>}
+        {price && <span className="label-large text-high-emphasis">${data?.price % 1 === 0 ? `${data?.price}.00` : data?.price}</span>}
       </div>
     </div>
   );
