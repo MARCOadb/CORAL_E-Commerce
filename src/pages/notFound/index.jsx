2d8fbd4e5941c18d483330738831a757022bfc31
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-08-16 01:22:38 -0300

    Responsive Improvments

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -2,24 +2,26 @@ import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./style.module.scss";
 import notFound from "../../assets/pics/Search/searchNotFound.png";
 import DefaultBtn from "../../components/defaultBtn";
+import useBreakpoint from "../../hooks/useBreakPoint";
 const NotFound = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { phone } = useBreakpoint()
   return (
     <>
       <div className={styles.notFoundContainer}>
         <img src={notFound} alt="" />
         <div className={styles.txtContainer}>
-          <span className="display-large ">
+          <span className={phone ? 'display-medium' : 'display-large'}>
             <span className="text-primary">404. </span>That's an error
           </span>
           <div style={{ display: "flex", flexDirection: "column" }}>
-            <span className="display-small ">The requested URL {location.pathname} was not found on this server.</span>
-            <span className="display-small ">That’s all we know.</span>
+            <span className={phone ? 'body-medium' : 'display-medium'}>The requested URL <span style={{ textDecoration: 'underline' }}>{location.pathname}</span> was not found on this server.</span>
+            <span className={phone ? 'body-medium' : 'display-medium'}>That’s all we know.</span>
           </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ display: "flex", justifyContent: "center", marginTop: '50px' }}>
             <DefaultBtn width={"70%"} onClick={() => navigate("/")}>
-              <span className="display-small">Go back to Home</span>
+              <span className={phone ? 'body-medium' : 'display-medium'}>Go back to Home</span>
             </DefaultBtn>
           </div>
         </div>
