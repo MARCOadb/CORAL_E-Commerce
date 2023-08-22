8d451cee0834b4f3e4a92e03b4efa243d5273eec
Vicentebp <vicentebpberwanger@gmail.com>
2023-08-22 15:30:17 -0300

    Ultimos fixes

--- src/components/mobileDrawer/index.jsx
+++ src/components/mobileDrawer/index.jsx
@@ -3,19 +3,19 @@ import Modal from "../modal";
 import profileImg from "../../assets/pics/Login/profile-default.png"; //placeholder
 import ArrowSvg from "../../assets/icon/ArrowSvg";
 import TextAndArrow from "../textAndArrow";
-import { useContext } from 'react'
+import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const MobileDrawer = ({ setOpen, open }) => {
-  const { user } = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const goToCategory = (category) => {
-    navigate('/categories', {
+    navigate("/categories", {
       state: {
         categoryOpen: true,
-        categoryName: category
+        categoryName: category,
       },
     });
   };
@@ -28,8 +28,8 @@ const MobileDrawer = ({ setOpen, open }) => {
           <>
             <div className="profileDiv">
               <div>
-                <img src={user?.profilePhoto === null ? profileImg : user?.profilePhoto} className="imgOutline" alt="nome"></img>
-                <span>{`Hello, ${user.firstName}`}</span>
+                <img src={user?.profilePhoto ? user?.profilePhoto : profileImg} className="imgOutline" alt="nome"></img>
+                <span>{`Hello, ${user?.firstName ? user.firstName : "Visitante"}`}</span>
               </div>
               <ArrowSvg stroke="black" x={-90} />
             </div>
@@ -38,14 +38,14 @@ const MobileDrawer = ({ setOpen, open }) => {
               <span>Top Categories</span>
             </div>
             <div className="listContainer">
-              <TextAndArrow x={-90} text="Skincare" onClick={(() => goToCategory("Skincare"))} />
-              <TextAndArrow x={-90} text="Apparels" onClick={(() => goToCategory("Apparels"))} />
-              <TextAndArrow x={-90} text="Jewellery" onClick={(() => goToCategory("Jewellery"))} />
-              <TextAndArrow x={-90} text="Handbags" onClick={(() => goToCategory("Handbags"))} />
-              <TextAndArrow x={-90} text="Eyewear" onClick={(() => goToCategory("Eyewear"))} />
-              <TextAndArrow x={-90} text="Fragrances" onClick={(() => goToCategory("Fragrances"))} />
-              <TextAndArrow x={-90} text="Watches" onClick={(() => goToCategory("Watches"))} />
-              <TextAndArrow x={-90} text="About" onClick={(() => navigate('/about'))} />
+              <TextAndArrow x={-90} text="Skincare" onClick={() => goToCategory("Skincare")} />
+              <TextAndArrow x={-90} text="Apparels" onClick={() => goToCategory("Apparels")} />
+              <TextAndArrow x={-90} text="Jewellery" onClick={() => goToCategory("Jewellery")} />
+              <TextAndArrow x={-90} text="Handbags" onClick={() => goToCategory("Handbags")} />
+              <TextAndArrow x={-90} text="Eyewear" onClick={() => goToCategory("Eyewear")} />
+              <TextAndArrow x={-90} text="Fragrances" onClick={() => goToCategory("Fragrances")} />
+              <TextAndArrow x={-90} text="Watches" onClick={() => goToCategory("Watches")} />
+              <TextAndArrow x={-90} text="About" onClick={() => navigate("/about")} />
             </div>
             <div className="separador"></div>
             <div className="subTitle">
